b4047ff3d3322f85ed6d0a08da341327acd7380d
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2024-08-06 16:58:49 +0800

    updates

--- pytorch tutorial/Training_a_Classifier.py
+++ pytorch tutorial/Training_a_Classifier.py
@@ -72,16 +72,26 @@ class Net(nn.Module):
         # in_channels, out_channels, kernel_size
         self.conv1 = nn.Conv2d(3, 6, 5)
         # torch.Size([4, 6, 28, 28])
+        # Applies a 2D max pooling over an input signal composed of several input planes.
+        # kernel_size, stride 
         self.pool = nn.MaxPool2d(2, 2)
+        # torch.Size([4, 6, 14, 14])
         self.conv2 = nn.Conv2d(6, 16, 5)
+        # torch.Size([4, 16, 10, 10])
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        # Applies an affine linear transformation to the incoming data
+        # in_features, out_features 
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
+        # relu : Applies the rectified linear unit function element-wise.
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
+        # torch.Size([4, 16, 5, 5])
         x = torch.flatten(x, 1) # flatten all dimensions except batch
+        # torch.Size([4, 400])
+        # start_dim is 1.
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
